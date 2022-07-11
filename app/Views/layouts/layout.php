ec9c5c296e92930c29a91e84ea293b325e3c096d
iAmWayne <iraolawayne@gmail.com>
2022-07-11 15:37:30 +0800

    initialized bootstrap

---
+++ app/Views/layouts/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('/src/css/app.css') ?>">
+    <title>Appointment System</title>
+</head>
+
+<body>
+    <script type="module" src="<?= base_url('/src/js/app.js') ?>"></script>
+</body>
+
+</html>
