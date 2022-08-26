e30266d97b8bb77328a90bf92f9e1a6d58da0bf2
doffie-JC <johdigay@my.cspc.edu.ph>
2022-08-26 11:15:48 +0800

    edit the comments and fix the return from model and controller

--- app/Controllers/Employee/EmployeeScanner.php
+++ app/Controllers/Employee/EmployeeScanner.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * This is a class for employee scanner process 
+ */
+
 namespace App\Controllers\Employee;
 
 use App\Controllers\BaseController;
@@ -7,30 +11,42 @@ use App\Models\EmployeeModel;
 
 class EmployeeScanner extends BaseController
 {
-    /**
-     * class: this is for testing qr code scanner
-     */
+
+    
+    private $employee_model;
+
+    //Instantiate
+    public function __construct()
+    {
+        $this->employee_model = new EmployeeModel();
+    }
 
     public function index(){
         // $data['test'] = $this->get_employee();
         return view('scanner');
     }
 
+    /**
+     * func: get and display all employee inside the database
+     * @return view with data (employees data)
+     */
     public function get_employee(){
-        $model = new EmployeeModel();
 
-        $data['employees'] = $model->get_all_employees();
+        $data['employees'] = $this->employee_model->get_all_employees();
         // return json_encode($data);
         return view('components/employee-list', $data);
     }
 
+    /**
+     * func: the proces where the data is from QR code
+     * @return string message(msg) to the user 
+     */
     public function track_employee(){
-        $model = new EmployeeModel();
 
         $employee_id = $this->request->getPost('emp_ID');
 
         if(!empty($employee_id)){
-            $reponse = $model->update_attendance_status($employee_id);
+            $reponse = $this->employee_model->update_attendance_status($employee_id);
 
             if($reponse == 1){
                 return json_encode([
@@ -48,5 +64,9 @@ class EmployeeScanner extends BaseController
                 ]);
             }
         }
+
+        return json_encode([
+            'msg' => 'Cannot Process'
+        ]);
     }
 }
--- app/Models/EmployeeModel.php
+++ app/Models/EmployeeModel.php
@@ -6,14 +6,19 @@ use CodeIgniter\Model;
 
 class EmployeeModel extends Model{
 
-    public function get_all_employees(){
-        /**
-         * return:array data
-         * func: get all the list of employee in the database
-         */
+    private $db_connect;
 
-         $db = \Config\Database::connect();
-         $query = $db->table('employee')
+    public function __construct(){
+        //instantiate
+        $this->db_connect = \Config\Database::connect();
+    }
+    /**
+     * func: get all the list of employee in the database
+     * @return $employee_data : array or list of employee 
+     */
+    public function get_all_employees(){
+
+         $query = $this->db_connect->table('employee')
             ->select('*')
             ->get();
         
@@ -21,31 +26,41 @@ class EmployeeModel extends Model{
         return $employee_data;
     }
 
+    /**
+     * func: update the status of the employee in the database
+     * @param $employee_id : int or string (all number) extract from qr code 
+     * @return int 1 = active, 0 = logout, 
+     * @return null  
+     */
     public function update_attendance_status($employee_id){
-        $db = \Config\Database::connect();
-        $query = $db->table('employee')
+
+        $query = $this->db_connect->table('employee')
             ->select('status')
             ->where('id', $employee_id)
             ->get()
             ->getRowArray();
-        
-        if($query['status'] == 0){
-            $update_query = $db->table('employee')
-                ->where('id', $employee_id)
-                ->update([
-                    'status' => 1
-                ]);
-            
-            return 1;
+
+        if(!empty($query)){
+            if($query['status'] == 0){
+                $this->db_connect->table('employee')
+                    ->where('id', $employee_id)
+                    ->update([
+                        'status' => 1
+                    ]);
+                
+                return 1;
+            }
+            else {
+                $this->db_connect->table('employee')
+                    ->where('id', $employee_id)
+                    ->update([
+                        'status' => 0
+                    ]);
+                
+                return 0;
+            }
         }
-        else {
-            $update_query = $db->table('employee')
-                ->where('id', $employee_id)
-                ->update([
-                    'status' => 0
-                ]);
-            
-            return 0;
-        }
+
+        return;
     }
 }
