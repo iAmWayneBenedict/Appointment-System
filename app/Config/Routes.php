9fcfdfeb3a91518e10433f85af622f977ccc6ce5
doffie-JC <johdigay@my.cspc.edu.ph>
2022-09-04 20:50:36 +0800

    updated base from controllers

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,11 +36,12 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::home');
+$routes->get('/test-sms', 'Home::index');
+$routes->post('/test-sms', 'Home::test_sms');
+
 
 $routes->get('/scanner', 'Employee\EmployeeScanner::index');
 $routes->post('/track-employee', 'Employee\EmployeeScanner::track_employee');
-$routes->post('/add-employee', 'Employee\EmployeeScanner::add_employee');
-$routes->match(['get', 'post'], '/get-employee-status', 'Employee\EmployeeScanner::get_employee_status');
 $routes->match(['get', 'post'], '/get-employee', 'Employee\EmployeeScanner::get_employee');
 
 $routes->group('user', static function ($routes) {
@@ -50,20 +51,15 @@ $routes->group('user', static function ($routes) {
     $routes->match(['get', 'post'], 'generate-id', 'End_Users\UserController::generate_user_id');
     $routes->post('register-user', 'End_Users\UserController::register_user');
     $routes->post('login-user', 'End_Users\UserLoginController::login_user');
+
+    $routes->group('dashboard', ['filter' => 'userFilter'], static function ($routes){
+        
+    });
 });
 
 $routes->group('admin', static function ($routes) {
-    $routes->get('login', 'Admin\Admin::login');
-    $routes->get('dashboard', 'Admin\Admin::index');
-    $routes->get('employees', 'Admin\Admin::employees');
-    $routes->get('qr-scanner', 'Admin\Admin::qr_scanner');
-    $routes->get('send-message', 'Admin\Admin::sendMessage');
-});
-
-$routes->group('employee', static function ($routes) {
-    $routes->get('index', 'Employee\Employee::index');
-    $routes->get('qr-generator', 'Employee\Employee::qr_generator');
-});
+    
+} );
 
 /*
  * --------------------------------------------------------------------
