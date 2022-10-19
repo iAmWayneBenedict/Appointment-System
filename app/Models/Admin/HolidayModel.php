7333df500490a1163f73620b0e1154aa223f135d
doffie-JC <johdigay@my.cspc.edu.ph>
2022-10-19 11:59:28 +0800

    fix holiday

--- app/Models/Admin/HolidayModel.php
+++ app/Models/Admin/HolidayModel.php
@@ -5,7 +5,7 @@ namespace App\Models\Admin;
 use App\Models;
 use CodeIgniter\Model;
 
-class HolidayModel  extends Model
+class HolidayModel extends Model
 {
 
     private $db_connect;
@@ -14,18 +14,6 @@ class HolidayModel  extends Model
     {
         //instantiate
         $this->db_connect = \Config\Database::connect();
-    }
-
-    public function  get_holidays()
-    {
-
-        $admin_query = $this->db_connect->table('holidays')
-            ->select('*')
-            ->get();
-
-        $data = $admin_query->getResultObject(); //object
-
-        return $data;
     }
 
     public function set_holidays($holiday_from, $holiday_to, $description)
@@ -40,5 +28,17 @@ class HolidayModel  extends Model
         ]);
 
         return $response;
+    }
+
+    public function get_holidays()
+    {
+
+        $admin_query = $this->db_connect->table('holidays')
+            ->select('*')
+            ->get();
+
+        $data = $admin_query->getResultObject(); //object
+
+        return $data;
     }
 }
